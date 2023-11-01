ab39a1c2eb47d68410de060237cc43ae8e89391a
Keaton-A-Ramey <93961620+Keaton-A-Ramey@users.noreply.github.com>
2023-11-01 12:11:38 -0400

    Todo Progress
    Basic styling, add and remove individual tasks complete.
    
    To do: Add "complete" button function

--- REACT/crud/src/App.js
+++ REACT/crud/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import {Task} from "./Task.js";
 
 function App() {
+
+  //const [todoList, setTodoList] = useState([]); WE NEED OBJECTS
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  };
+
+  const addTask = () => {
+    // Doing this with objects...
+    // setTodoList([...todoList, newTask]);
+
+    const task = {
+      // We're adding these, having IDs and using ternary to handle first addition.
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1, 
+      taskName: newTask,
+      completed: false
+    };
+
+    setTodoList([...todoList,task])
+  };
+
+  const deleteTask = (taskID) => {
+    /*
+    // This works but its a little too long...
+    const newArr = todoList.filter((task) => {
+      return task !== taskName;
+    })
+    */
+    // This is cleaner, but it DOES delete all instances of the taskName from the list. 
+    // We're making objects with IDs
+    //setTodoList(todoList.filter((task) => task !== taskName));
+    setTodoList(todoList.filter((task) => task.id !== taskID));
+  }
+
+  const taskComplete = (props) => {
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="addTask">
+        <input onChange={handleChange}/>
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className="taskList">
+        {todoList.map((task) =>{
+          return <Task taskName = {task.taskName} id={task.id} deleteTask={deleteTask}/>;
+        })}
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
